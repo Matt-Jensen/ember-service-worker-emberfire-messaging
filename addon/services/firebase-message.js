f01f4d778fe5b7b758893b3ca1f25bc873379588
Matthew Jensen <matt@jwc.works>
2017-11-18 12:28:36 -0600

    fix firebase-messaging linting, documentation, privatize firebase messaging instance, and dependency import nomenclature

--- addon/services/firebase-message.js
+++ addon/services/firebase-message.js
@@ -1,19 +1,31 @@
-import RSVP from 'rsvp';
+import RSVP, { reject } from 'rsvp';
 import Service from '@ember/service';
-import { inject } from '@ember/service';
+import { inject as service } from '@ember/service';
 import { get, set } from '@ember/object';
 import EmberError from '@ember/error';
 import { assert } from '@ember/debug';
 import { run } from '@ember/runloop';
+import { getOwner } from '@ember/application';
+import { computed } from '@ember/object';
+import { oneWay } from '@ember/object/computed';
 
 export default Service.extend({
-  firebaseApp: inject(),
+  firebaseApp: service(),
 
   /**
    * Firebase messaging instance
    * @type {firebase.messaging.Messaging}
    */
-  messaging: null,
+  _messaging: computed(function() {
+    return get(this, 'firebaseApp').messaging();
+  }),
+
+  /**
+   * `_messaging` proxy with deprecation warning
+   * @type {firebase.messaging.Messaging}
+   * @TODO: add dep notice and remove in major release
+   */
+  messaging: oneWay('_messaging'),
 
   /**
    * onMessage event subscribers
@@ -26,11 +38,17 @@ export default Service.extend({
    * @type {String}
    */
   token: '',
-  _fastboot: computed(function () {
-    let owner = getOwner(this);
 
-    return owner.lookup('service:fastboot');
+  /**
+   * Is Fastboot env
+   * @return {Boolean}
+   */
+  _isFastboot: computed(function () {
+    return Boolean(
+      get(getOwner(this).lookup('service:fastboot') || {}, 'isFastBoot')
+    );
   }),
+
   /**
    * Add subscriber to `onMessage` event
    * @param  {Function} fn
@@ -65,24 +83,22 @@ export default Service.extend({
    */
   initialize() {
     return this.serviceWorkerReady().then(() => {
-      const messaging = get(this, 'messaging');
-      return messaging.requestPermission()
-      .then(this.getToken.bind(this));
+      return get(this, '_messaging').requestPermission()
+        .then(this.getToken.bind(this));
     });
   },
 
   /**
    * Proxy for: firebase `Messaging.getToken()`
-   * @return {Promise}
-   * @resolve {String} token
+   * @return {Promise} - resolves {String} token
    */
   getToken() {
-    return get(this, 'messaging').getToken()
+    return get(this, '_messaging').getToken()
     .then(token => {
       if (token) {
         return set(this, 'token', token);
       } else {
-        return RSVP.Promise.reject(
+        return reject(
           new EmberError('No Instance ID token available. Request permission to generate one.')
         );
       }
@@ -91,49 +107,41 @@ export default Service.extend({
 
   init() {
     this._super(...arguments);
-    if (get(this, '_fastboot.isFastBoot')) {
-      return;
+
+    if (get(this, '_isFastboot') === false) {
+
+      /*
+       Invalidate and request a new token when invalidated by FCM
+       */
+      get(this, '_messaging').onTokenRefresh(() => {
+        set(this, 'token', '');
+        this.getToken();
+      });
+
+      /**
+       * Invoke subscribers queue `onMessage`
+       * @param {Object} payload
+       *
+       * Example payload:
+       * {
+       *   collapse_key: "do_not_collapse",
+       *   from: "463017952018",
+       *   notification: {
+       *     body: "5 to 1",
+       *     click_action: "http://localhost:4200",
+       *     icon : "sparkle-logo.svg",
+       *     title: "Portugal vs. Denmark"
+       *   }
+       * }
+       */
+      get(this, '_messaging').onMessage((payload) =>
+        get(this, '_subscribers').forEach(fn => fn(payload)));
     }
-    /*
-     Set `messaging` instance
-     */
-    const messaging = set(
-      this,
-      'messaging',
-      get(this, 'firebaseApp').messaging()
-    );
-
-    /*
-     Invalidate and request a new token when invalidated by FCM
-     */
-    messaging.onTokenRefresh(() => {
-      set(this, 'token', '');
-      this.getToken();
-    });
-
-    /**
-     * Invoke subscribers queue `onMessage`
-     * @param {Object} payload
-     *
-     * Example payload:
-     * {
-     *   collapse_key: "do_not_collapse",
-     *   from: "463017952018",
-     *   notification: {
-     *     body: "5 to 1",
-     *     click_action: "http://localhost:4200",
-     *     icon : "sparkle-logo.svg",
-     *     title: "Portugal vs. Denmark"
-     *   }
-     * }
-     */
-    messaging.onMessage((payload) =>
-      get(this, '_subscribers').forEach(fn => fn(payload)));
   },
 
   /**
    * Resolve once service worker successfully registered
-   * @return {Promise}
+   * @return {RSVP.Promise}
    */
   serviceWorkerReady() {
     return new RSVP.Promise((resolve, reject) => {
